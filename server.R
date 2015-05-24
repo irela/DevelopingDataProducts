457a746b975d425f90fc4c67f606dfb24d736890
Eiri <eiri.valanto@gmail.com>
2015-05-24 11:06:52 +0300

    App ready for publish

--- server.R
+++ server.R
@@ -1,15 +1,24 @@
 library(UsingR)
-data(galton)
+data(nottem)
+#temps <- as.data.frame(tapply(nottem, list(year = floor(time(nottem)), month = cycle(nottem)), c))
+Temperature <- tapply(nottem, list(year = floor(time(nottem)), month = cycle(nottem)), c)
+mse <- rep(0, 12)
+months <- c("January", "February", "March", "April", "May", "June", "July", "August",
+            "September", "October", "November", "December") 
 
 shinyServer(
   function(input, output) {
     output$newHist <- renderPlot({
-      hist(galton$child, xlab='child height', col='lightblue',main='Histogram')
+      hist(Temperature, xlab='Temperature', col='lightblue',main='Histogram of monthly average temperatures')
       mu <- input$mu
-      lines(c(mu, mu), c(0, 200),col="red",lwd=5)
-      mse <- mean((galton$child - mu)^2)
-      text(63, 150, paste("mu = ", mu))
-      text(63, 140, paste("MSE = ", round(mse, 2)))
+      lines(c(mu, mu), c(0, 50),col="red",lwd=5)
+      for (i in 1:12) {
+        mse[i] <- mean((Temperature[,i]- mu)^2)
+      }
+      
+      text(60, 55, paste("Temperature = ", mu))
+      text(60, 52, paste("Most probable month = ", months[which.min(mse)]))
+      text(60, 49, paste("Most unlikely month = ", months[which.max(mse)]))      
     })
     
   }
--- ui.R
+++ ui.R
@@ -1,8 +1,9 @@
 shinyUI(pageWithSidebar(
-  headerPanel("Example plot"),
+  headerPanel("Nottingham Monthly Temperatures"),
   sidebarPanel(
-    sliderInput('mu', 'Guess at the mu',value = 70, min = 60, max = 80, step = 0.05,),
-    h4('Choose a value using the slider and see mu and mean on the histogram.')
+    sliderInput('mu', 'Select temperature',value = 50, min = 30, max = 70, step = 0.05,),
+    h4('Choose a temperature using the slider and see which month\'s temperature in Nottingham  
+       is closest to the chosen temperature. Also the most unlikely month for that temperature is shown.')
   ),
   mainPanel(
     plotOutput('newHist')
